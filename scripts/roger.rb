8a0093bc8b8ca65f197cd903742836505dd3aba7
Matus Kmit <matus.kmit@zhdk.ch>
2020-02-25 16:19:17 +0100

    wip

---
+++ scripts/roger.rb
@@ -0,0 +1,11 @@
+# run with "rails runner -e production PATH_TO_THIS_FILE"
+# WARNING: USE AT YOUR OWN RISK!!!
+
+require_relative('shared/logger')
+# require_relative('shared/parse_csv')
+require('pry')
+
+AccessRight
+  .where(inventory_pool_id: "b534b07c-d9bf-4572-840c-0a630b8cd8ed", role: "customer")
+  .joins("INNER JOIN reservations ON reservations.user_id = access_rights.user_id AND reservations.status != 'signed'")
+  .delete_all
